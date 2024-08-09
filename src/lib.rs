2388d8b8b3ab4d90b21c6fababed3fd91c05913c
stompesi <stompesi@gmail.com>
2024-08-09 15:40:38 +0900

    Remove no need file (binary)

--- src/lib.rs
+++ src/lib.rs
@@ -70,25 +70,26 @@ mod tests {
         let max_sequencer_number = BigUint::from(MAX_SEQUENCER_NUMBER);
 
         let skde_params = setup(time, p, q, g, max_sequencer_number);
-
         let message: &str = "12345";
 
+        // 1. Generate partial keys and proofs
         let generated_keys_and_proofs: Vec<_> = (0..MAX_SEQUENCER_NUMBER)
             .enumerate()
             .map(|(index, _)| {
                 let start = Instant::now();
                 let (secret_value, extraction_key) = generate_partial_key(&skde_params);
-                let partial_key_proof = prove_partial_key_validity(&skde_params, &secret_value);
+                let key_proof = prove_partial_key_validity(&skde_params, &secret_value);
                 let generation_duration = start.elapsed();
                 println!(
                     "Sequencer{}'s key and proof generation time: {:?}",
                     index + 1,
                     generation_duration
                 );
-                (extraction_key, partial_key_proof)
+                (extraction_key, key_proof)
             })
             .collect();
 
+        // 2. Verify all generated keys
         let verification_start = Instant::now();
         generated_keys_and_proofs
             .iter()
@@ -109,39 +110,36 @@ mod tests {
             MAX_SEQUENCER_NUMBER, verification_duration
         );
 
-        let extraction_keys: Vec<_> = generated_keys_and_proofs
+        let partial_key_list = generated_keys_and_proofs
             .into_iter()
-            .map(|(extraction_key, _)| extraction_key)
+            .map(|(partial_key, _)| partial_key)
             .collect();
 
-        // Aggregate all generated keys
+        // 3. Aggregate all partial keys
         let aggregation_start = Instant::now();
-        let aggregated_key = aggregate_key(&skde_params, &extraction_keys);
+        let aggregated_key = aggregate_key(&skde_params, &partial_key_list);
         let aggregation_duration = aggregation_start.elapsed();
         println!("Aggregation time: {:?}", aggregation_duration);
 
-        let public_key = PublicKey {
+        let encryption_key = PublicKey {
             pk: aggregated_key.u.clone(),
         };
 
+        // 4. Encrypt the message
         let encryption_start = Instant::now();
-
-        let cipher_text = encrypt(&skde_params, message, &public_key).unwrap();
-
+        let cipher_text = encrypt(&skde_params, message, &encryption_key).unwrap();
         let encryption_duration = encryption_start.elapsed();
         println!("Encryption time: {:?}", encryption_duration);
 
+        // 5. Solve the time-lock puzzle
         let puzzle_start = Instant::now();
-
         let secret_key = solve_time_lock_puzzle(&skde_params, &aggregated_key).unwrap();
-
         let puzzle_duration = puzzle_start.elapsed();
         println!("Puzzle solved time: {:?}", puzzle_duration);
 
+        // 6. Decrypt the cipher text
         let decryption_start = Instant::now();
-
         let decrypted_message = decrypt(&skde_params, &cipher_text, &secret_key).unwrap();
-
         let decryption_duration = decryption_start.elapsed();
         println!("Decryption time: {:?}", decryption_duration);
 
