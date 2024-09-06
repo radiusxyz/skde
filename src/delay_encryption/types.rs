22e758f69c9125c9f0c9a3e60539dc348a766e11
stompesi <stompesi@gmail.com>
2024-09-06 19:03:11 +0900

    Add serialize and deserialize in PublicKey

--- src/delay_encryption/types.rs
+++ src/delay_encryption/types.rs
@@ -1,6 +1,7 @@
 use num_bigint::BigUint;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct PublicKey {
     pub pk: BigUint,
 }
