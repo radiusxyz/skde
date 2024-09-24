82e27cbf2bf2b22002be3eab92963b0e40c18564
stompesi <stompesi@gmail.com>
2024-09-24 23:19:13 +0900

    Add radix

--- src/delay_encryption/encryption.rs
+++ src/delay_encryption/encryption.rs
@@ -2,6 +2,7 @@ use super::{CipherPair, PublicKey, SecretKey};
 use crate::SkdeParams;
 use big_integer::{big_mod_inv, big_mul_mod, big_pow_mod};
 use num_bigint::{BigUint, RandBigInt};
+use num_traits::Num;
 use rand::thread_rng;
 use std::{
     io::{self, ErrorKind},
@@ -12,9 +13,15 @@ pub fn encrypt(
     skde_params: &SkdeParams,
     message: &str,
     encryption_key: &PublicKey,
+    radix: u32,
 ) -> io::Result<CipherPair> {
     // TODO: Arbitrary Length of Message
-    let plain_text = BigUint::from_str(message).expect("Invalid message");
+    let plain_text;
+    if message.starts_with("0x") {
+        plain_text = BigUint::from_str_radix(&message[2..], radix).expect("Invalid message");
+    } else {
+        plain_text = BigUint::from_str_radix(message, radix).expect("Invalid message");
+    }
 
     if plain_text >= skde_params.n {
         return Err(io::Error::new(
@@ -41,6 +48,7 @@ pub fn decrypt(
     skde_params: &SkdeParams,
     cipher_text: &CipherPair,
     decryption_key: &SecretKey,
+    radix: u32,
 ) -> io::Result<String> {
     let cipher1 = BigUint::from_str(&cipher_text.c1).unwrap();
     let cipher2 = BigUint::from_str(&cipher_text.c2).unwrap();
@@ -52,5 +60,5 @@ pub fn decrypt(
 
     let result = (cipher2 * inv_mod) % &skde_params.n;
 
-    Ok(result.to_str_radix(10))
+    Ok(result.to_str_radix(radix))
 }
