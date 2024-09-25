dbadf8d183632d8ac116bbb5c357e003ac5080e1
kanet <zombiedelah@gmail.com>
2024-09-25 18:07:44 +0900

    'encrypt()' can take &str regardless of its length.

--- src/delay_encryption/encryption.rs
+++ src/delay_encryption/encryption.rs
@@ -1,75 +1,46 @@
-use super::{CipherPair, PublicKey, SecretKey};
-use crate::SkdeParams;
+use std::{fmt::Write, str::FromStr};
+
 use big_integer::{big_mod_inv, big_mul_mod, big_pow_mod};
 use num_bigint::{BigUint, RandBigInt};
 use num_traits::Num;
 use rand::thread_rng;
-use std::{
-    io::{self, ErrorKind},
-    str::FromStr,
+
+use crate::{
+    delay_encryption::{CipherPair, Ciphertext, PublicKey, SecretKey},
+    SkdeParams,
 };
 
-/* 
-TODO: DH
-encrypt(
-    skde_params: &SkdeParams,
-    message: &str, // "안ㄴ녕하세요 저는 한ㅁ니ㅏ어리머니ㅏㅇ러ㅏ미ㅏ넝리ㅓㅁ닝러ㅏㅣㅁㄴ" // "1231" // "0x1111" / "{"from": "0x123", "to": "0x123", "value": 123}"
-    encryption_key: &PublicKey,
-) -> io::Result<String> // Vec<CipherPair>의 string화
-
-let encrypted_data = format!("{}/{}", encrypted_data.c1, encrypted_data.c2);
-let encrypted_data = format!("{},", encrypted_data);
-
-pub fn decrypt(
-    skde_params: &SkdeParams,
-    encrypted_data: &str,
-    decryption_key: &SecretKey,
-) -> io::Result<String> {
- 
-let encrypted_data = format!("{},", encrypted_data);
-let encrypted_data = format!("{}/{}", encrypted_data.c1, encrypted_data.c2);
-
-concat
-
-*/
+/// TODO: Modify chunk size to increase performance.
 pub fn encrypt(
     skde_params: &SkdeParams,
-    message: &str, // "안ㄴ녕하세요 저는 한ㅁ니ㅏ어리머니ㅏㅇ러ㅏ미ㅏ넝리ㅓㅁ닝러ㅏㅣㅁㄴ" // "1231" // "0x1111" / "{"from": "0x123", "to": "0x123", "value": 123}"
+    message: impl AsRef<str>,
     encryption_key: &PublicKey,
-) -> io::Result<String> {
-    // String -> bytes -> Hex String (어떤수)
-    let message_hex_string = string_to_hex(message);
+) -> Result<Ciphertext, EncryptionError> {
+    let cipher_pair_list: Result<Vec<CipherPair>, EncryptionError> = message
+        .as_ref()
+        .as_bytes()
+        .chunks(64)
+        .map(|slice| encrypt_slice(skde_params, slice, encryption_key))
+        .collect();
 
-    let plain_text = BigUint::from_str_radix(&message_hex_string, 16).expect("Invalid message");
+    Ok(cipher_pair_list?.into())
+}
 
-    // 123123123123123123123234234234234
-
-    // skde_params.n = 12312312312312 -> 14 -> 13
-    // 12312312312312 -> encryptedTx
-    // 31231232342342 -> encryptedTx
-    // 342340x2312323 -> encryptedTx
-    // 4234234234     -> encryptedTx
-    //-> Vec<CipherPair>
-
-    // TODO: Arbitrary Length of Message
-    // TODO: Support for message length greater than N
-    if plain_text >= skde_params.n {
-        // 어떤수가 길이만큼 잘라야함
-        return Err(io::Error::new(
-            ErrorKind::Other,
-            "Message must be less than modular size",
-        ));
-    }
+fn encrypt_slice(
+    skde_params: &SkdeParams,
+    slice: &[u8],
+    encryption_key: &PublicKey,
+) -> Result<CipherPair, EncryptionError> {
+    let message_str = std::str::from_utf8(slice).map_err(EncryptionError::InvalidUtf8)?;
+    let message_hex_string = const_hex::encode(message_str);
+    let plain_text =
+        BigUint::from_str_radix(&message_hex_string, 16).map_err(EncryptionError::ParseBigUint)?;
 
     let mut rng = thread_rng();
-
-    // choose a random which is less than N/2
     let l: BigUint = rng.gen_biguint(skde_params.n.bits() / 2);
     let pk_pow_l = big_pow_mod(&encryption_key.pk, &l, &skde_params.n);
     let cipher1 = big_pow_mod(&skde_params.g, &l, &skde_params.n);
     let cipher2 = big_mul_mod(&plain_text, &pk_pow_l, &skde_params.n);
-
-    Vec<CipherPair>
 
     Ok(CipherPair {
         c1: cipher1.to_str_radix(10),
@@ -77,62 +48,71 @@ pub fn encrypt(
     })
 }
 
-// 4234234234     -> //-> Vec<CipherPair>
-// 12312312312312 <- encryptedTx
-// 31231232342342 <- encryptedTx
-// 342340x2312323 <- encryptedTx
-// 4234234234     <- encryptedTx
-// => // 123123123123123123123234234234234
+#[derive(Debug)]
+pub enum EncryptionError {
+    InvalidUtf8(std::str::Utf8Error),
+    ParseBigUint(num_bigint::ParseBigIntError),
+}
+
+impl std::fmt::Display for EncryptionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for EncryptionError {}
+
 pub fn decrypt(
     skde_params: &SkdeParams,
-    cipher_text_list: &Vec<CipherPair>,
+    ciphertext: &Ciphertext,
     decryption_key: &SecretKey,
-) -> io::Result<String> {
-    let mut message_hex_string = String::new();
+) -> Result<String, DecryptionError> {
+    let mut message = String::new();
+    ciphertext.iter().try_for_each(|cipher_pair| {
+        decrypt_inner(&mut message, skde_params, cipher_pair, decryption_key)
+    })?;
 
-    for let cipher_text in cipher_text_list {
-        let cipher1 = BigUint::from_str(&cipher_text.c1).unwrap();
-        let cipher2 = BigUint::from_str(&cipher_text.c2).unwrap();
+    let message_vec = const_hex::decode(message).map_err(DecryptionError::DecodeHexString)?;
+    let message_string = String::from_utf8(message_vec).map_err(DecryptionError::InvalidUtf8)?;
 
-        let exponentiation = big_pow_mod(&cipher1, &decryption_key.sk, &skde_params.n);
+    Ok(message_string)
+}
 
-        let inv_mod = big_mod_inv(&exponentiation, &skde_params.n)
-            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "No modular inverse found"))?;
+fn decrypt_inner(
+    message: &mut String,
+    skde_params: &SkdeParams,
+    cipher_pair: &CipherPair,
+    decryption_key: &SecretKey,
+) -> Result<(), DecryptionError> {
+    let cipher_1 = BigUint::from_str(&cipher_pair.c1).map_err(DecryptionError::ParseBigUint)?;
+    let cipher_2 = BigUint::from_str(&cipher_pair.c2).map_err(DecryptionError::ParseBigUint)?;
 
-        let result = (cipher2 * inv_mod) % &skde_params.n;
-        message_hex_string += result.to_str_radix(16);
+    let exponentiation = big_pow_mod(&cipher_1, &decryption_key.sk, &skde_params.n);
 
+    let inv_mod = big_mod_inv(&exponentiation, &skde_params.n)
+        .ok_or(DecryptionError::NoModularInverseFound)?;
+
+    let output = ((cipher_2 * inv_mod) % &skde_params.n).to_str_radix(16);
+    message
+        .write_str(&output)
+        .map_err(DecryptionError::WriteMessage)?;
+
+    Ok(())
+}
+
+#[derive(Debug)]
+pub enum DecryptionError {
+    ParseBigUint(num_bigint::ParseBigIntError),
+    NoModularInverseFound,
+    WriteMessage(std::fmt::Error),
+    DecodeHexString(const_hex::FromHexError),
+    InvalidUtf8(std::string::FromUtf8Error),
+}
+
+impl std::fmt::Display for DecryptionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
     }
-
-    // let message = hex_to_string(&message_hex_string).unwrap();
-
-    // let cipher1 = BigUint::from_str(&cipher_text.c1).unwrap();
-    // let cipher2 = BigUint::from_str(&cipher_text.c2).unwrap();
-
-    // let exponentiation = big_pow_mod(&cipher1, &decryption_key.sk, &skde_params.n);
-
-    // let inv_mod = big_mod_inv(&exponentiation, &skde_params.n)
-    //     .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "No modular inverse found"))?;
-
-    // let result = (cipher2 * inv_mod) % &skde_params.n;
-    // let message_hex_string = result.to_str_radix(16);
-
-    let message = hex_to_string(&message_hex_string).unwrap();
-
-    Ok(message)
 }
 
-fn string_to_hex(s: &str) -> String {
-    let vec: Vec<u8> = s.as_bytes().to_vec(); // 문자열을 벡터로 변환
-    let hex_string: String = vec.iter().map(|byte| format!("{:02x}", byte)).collect();
-    hex_string
-}
-
-fn hex_to_string(hex: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let bytes: Vec<u8> = (0..hex.len())
-        .step_by(2)
-        .map(|i| u8::from_str_radix(&hex[i..i + 2], 16))
-        .collect::<Result<Vec<u8>, _>>()?;
-    let result_string = String::from_utf8(bytes)?;
-    Ok(result_string)
-}
+impl std::error::Error for DecryptionError {}
--- src/delay_encryption/types.rs
+++ src/delay_encryption/types.rs
@@ -1,18 +1,33 @@
 use num_bigint::BigUint;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct PublicKey {
     pub pk: BigUint,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct SecretKey {
     pub sk: BigUint,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize)]
 pub struct CipherPair {
     pub c1: String,
     pub c2: String,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Ciphertext(Vec<CipherPair>);
+
+impl From<Vec<CipherPair>> for Ciphertext {
+    fn from(value: Vec<CipherPair>) -> Self {
+        Self(value)
+    }
+}
+
+impl Ciphertext {
+    pub fn iter(&self) -> core::slice::Iter<'_, CipherPair> {
+        self.0.iter()
+    }
 }
